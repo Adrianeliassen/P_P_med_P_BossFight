a182c30251068c5bc7b9d117b034e27d571815b9
Adrianeliassen <122351623+Adrianeliassen@users.noreply.github.com>
2023-06-07 15:38:53 +0200

    Add project files.

---
+++ P_P_med_P_BossFight/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_P_med_P_BossFight
+{
+    public class Player
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Strength { get; private set; }
+        public int Stamina { get; private set; }
+        public bool IsDead { get; private set; }
+        public bool IsBoss { get; private set; }
+        public int BaseStamina { get; private set; }
+
+        public Player(string name, int health, int strength, int stamina, bool isBoss)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            IsBoss = isBoss;
+            BaseStamina = stamina;
+        }
+        public void Recharge()
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(500);
+            });
+            t.Wait();
+
+            Stamina = BaseStamina;
+            Console.WriteLine($"{Name} is out of stamina, recharging");
+        }
+        public void Fight(Player player)
+        {
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(500);
+            });
+            t.Wait();
+
+            int AttackDamage = Strength;
+            if(IsBoss)
+            {
+                Random r = new Random();
+                AttackDamage = r.Next(0, Strength);
+            }
+            if (Stamina >= 10) 
+            {
+                
+                player.Health -= AttackDamage;
+                Stamina -= 10;
+                Console.WriteLine($"\n\n{Name} attacked {player.Name} for {AttackDamage}HP. {player.Name} has {player.Health}HP remaining.");
+            } else {
+                Recharge();
+            };
+            
+        }
+
+        public void Die()
+        {
+            Console.WriteLine($"{Name} died");
+            IsDead = true;
+        }
+    }
+}
---
+++ P_P_med_P_BossFight/Program.cs
@@ -0,0 +1,41 @@
+﻿using P_P_med_P_BossFight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_P_med_P_BossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Runfight();
+        }
+        public static void Runfight()
+        {
+            Console.WriteLine("Lets fight! Press any key to start fighting");
+            string startFight = Console.ReadLine();
+            Player hero = new Player("Hero", 100, 20, 40, false);
+            Player boss = new Player("Boss", 400, 20, 10, true);
+
+            while (!(boss.IsDead || hero.IsDead)) Fight(hero, boss);
+        }
+        private static void Fight(Player hero, Player boss)
+        {
+            hero.Fight(boss);
+            if(boss.Health <= 0 )
+            {
+                boss.Die();
+                return;
+            }
+            boss.Fight(hero);
+            if (hero.Health <= 0) hero.Die();
+        }
+    }
+    
+}
+//Random rand = new Random();
+//Strength = rand.Next(0,30);
+//while (Hero.Health == 0 || Boss.health == 0) {}
